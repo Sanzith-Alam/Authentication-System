84d8750563e40a8acc2dfb6c2b1ee94996954031
Sanzith <sanzithalam11@gmail.com>
2025-03-11 01:20:51 +0600

    email

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,6 @@
-
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken';
+import userModel from '../models/userModel.js';
 
 export const register = async (requestAnimationFrame,res)=>{
     const {name, email, password}= req.body;
@@ -7,6 +9,17 @@ export const register = async (requestAnimationFrame,res)=>{
     }
     
     try{
+
+        const existingUser = await userModel.findOne({email})
+
+        if(existingUser){
+            return res.json({success: false, message: "User already exists"});
+        }
+
+        const hasedPassword =await bcrypt.hash(password,10);
+
+        const user = new userModel({name, email, password:hasedPassword });
+        await user.save();
 
     }catch(error){
         res.json({success: false, message: error.message})

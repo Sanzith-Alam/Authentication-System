28c19a567d55db801c265eb1a1d060262c457ba2
Sanzith <sanzithalam11@gmail.com>
2025-03-10 15:15:11 +0600

    first

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
-import express from "express";
-import cors from 'cors'
-import 'dotenv/config';
-import cookieParser  from "cookie-parser";
+import express, { request } from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 
-const app =express ();
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true}))
+app.use(cors({ credentials: true }))
 
-app.listen(port, ()=> console.log('server started on PORT:${port}'));
+app.get('/',(req, res)=>res.send("API Working"));
+
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));

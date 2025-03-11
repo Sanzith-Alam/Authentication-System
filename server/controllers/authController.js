9ab3bff59de3dca6c1dd6e4f1585ad979ac4014b
Sanzith <sanzithalam11@gmail.com>
2025-03-12 00:36:46 +0600

    nodemailer

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
+import transporter from '../config/nodemailer.js';
 
 export const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -29,6 +30,14 @@ export const register = async (req, res) => {
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
+
+        const mailOptions={
+            from: process.env.SENDER_EMAIL, 
+            to: email,
+            subject: 'Welcome',
+            text: 'Your account has been created with email id: ${email}'
+        }
+        await transporter.sendMail(mailOptions);
 
         return res.json({ success: true });
     } catch (error) {

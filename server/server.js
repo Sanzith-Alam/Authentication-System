d33872fc29263b698692c1883ef6ab9fb96c1d0a
Sanzith <sanzithalam11@gmail.com>
2025-03-12 23:48:59 +0600

    complete backend

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
 import authRouther from './routes/authRoutes.js'
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,5 +20,6 @@ app.use(cors({ credentials: true }))
 //Api Endpoints
 app.get('/',(req, res)=>res.send("API Working fine"));
 app.use('/api/auth', authRouther)
+app.use('/api/user', userRouter)
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
